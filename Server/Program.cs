5f37ffea1a6b7938db1725d4e3005a803bee2497
d1orn <zhinovnikita@yandex.ru>
2025-05-11 16:13:25 +0700

    add using cors

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.Run();
