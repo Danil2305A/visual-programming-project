1cd5bcd00daeab6c64792584801e2a5c43e5715a
hikaruvi <daniilrybkin32@gmail.com>
2025-05-17 18:22:08 +0700

    add initial implementation

---
+++ Server/Repository/User/IUserRepository.cs
@@ -0,0 +1,11 @@
+public interface IUserRepository
+{
+  Task Add(User user);
+  Task<User> Get(Guid id);
+  Task<User> Get(string email);
+  Task<IEnumerable<User>> GetAll();
+  Task Update(User user);
+  Task Remove(Guid id);
+}
+
+// db context у нас scope, не singleton
