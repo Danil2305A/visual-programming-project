a1e06f9fe1a0b2b50b5202c1e27ec10c9d9c6217
limbro55 <limbro55@mail.ru>
2025-05-18 12:25:54 +0700

    make review

---
+++ Server/Service/ReviewService.cs
@@ -0,0 +1,28 @@
+﻿using Server.Model;
+using Server.Repository;
+
+namespace Server.Service
+{
+    public class ReviewService
+    {
+        private readonly IReviewRepository _repository;
+
+        public ReviewService(IReviewRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Review> GetAll() => _repository.GetAll();
+
+        // Заменяем int на Guid
+        public Review? Get(Guid id) => _repository.Get(id);
+
+        public bool Add(Review review) => _repository.Add(review);
+
+        // Заменяем int на Guid
+        public bool Update(Guid id, Review updated) => _repository.Update(id, updated);
+
+        // Заменяем int на Guid
+        public bool Remove(Guid id) => _repository.Remove(id);
+    }
+}
